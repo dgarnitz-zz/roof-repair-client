908cfd1395466211b3764da91435b6eaea408203
dag8 <dag8@pc5-013-l.cs.st-andrews.ac.uk>
2019-06-17 12:45:26 +0100

    made some slight adjustments to the uploader. will now experiment with moving the functions up to the parent

--- src/components/Uploader.js
+++ src/components/Uploader.js
@@ -1,8 +1,17 @@
 import React, { Component } from 'react';
 import ImageUploader from 'react-images-upload';
+import Fab from '@material-ui/core/Fab';
+import { makeStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
 
 {/* This uses the react-image-upload component, created by Jake Hartnell
  https://github.com/jakehartnell/react-images-upload#readme */}
+
+ const useStyles = makeStyles(theme => ({
+    root: {
+      flexGrow: 1,
+    }
+  }));
  
 class Uploader extends Component {
  
@@ -10,24 +19,44 @@ class Uploader extends Component {
         super(props);
          this.state = { pictures: [] };
          this.onDrop = this.onDrop.bind(this);
+         this.getPrice = this.getPrice.bind(this);
     }
  
+    
     onDrop(picture) {
         this.setState({
             pictures: this.state.pictures.concat(picture),
         });
     }
+
+    /* This will probably need to be moved to the parent so the data it receives back and be rendered */
+    getPrice(){
+        console.log(this.state.pictures);
+        this.setState({pictures: [] })
+    }
+
+    
  
     render() {
+
         return (
-            <ImageUploader
-                withIcon={true}
-                buttonText='Choose images'
-                onChange={this.onDrop}
-                imgExtension={['.jpg', '.gif', '.png', '.gif']}
-                maxFileSize={5242880}
-                label="Max file size: 5mb, formats accepted: jpg, gif, png"
-            />
+            <Grid container className={useStyles.root} spacing={2}>
+                <Grid item xs={12}>
+                    <ImageUploader
+                        withIcon={true}
+                        buttonText='Choose image'
+                        onChange={this.onDrop}
+                        imgExtension={['.jpg', '.gif', '.png', '.gif']}
+                        maxFileSize={5242880}
+                        label="Max file size: 5mb, formats accepted: jpg, gif, png"
+                    />
+                </Grid>
+                <Grid item xs={12}>
+                    <Fab variant="extended" onClick = {this.getPrice}>
+                        Get price!
+                    </ Fab>
+                </Grid>
+            </Grid>
         );
     }
 }
