04348ed654eea7cdcc377b7c97018c7653c93e9a
dag8 <dag8@pc5-013-l.cs.st-andrews.ac.uk>
2019-06-17 11:04:07 +0100

    added a spinner component but I probably won't end up using it. Also added an entire upload component using npm react-images-upload. The labels are poorly written and should probably be overwritten. If you decide to do it later, the source code for the component is on github.

--- src/routes/Main.js
+++ src/routes/Main.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
 import Header from '../components/Header';
+import Spinner from '../components/Spinner';
+import Uploader from '../components/Uploader';
 
 class Main extends Component {
     render(){
         return (
             <div>
                 <Header />
-                <p>This is a test</p>
-                <input type='file' id='multi' />
+                <Uploader />
             </div>
             
         );
