895a56548afe2ca9012bf9898aa2d172c4cc0464
dag8 <dag8@pc5-018-l.cs.st-andrews.ac.uk>
2019-07-22 14:54:19 +0100

    added first unit test using enyzme, which runs on Chai for assertion. Checks if the component renders properly

--- src/components/Header.test.js
+++ src/components/Header.test.js
@@ -2,6 +2,15 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import Header from './Header';
 
-it('renders without crashing', () => {
-  shallow(<Header />);
+import { configure } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+
+configure({ adapter: new Adapter() })
+
+describe('<Header />', () => {
+  it('renders without crashing', () => {
+    shallow(<Header />);
+  });
 });
+
+
